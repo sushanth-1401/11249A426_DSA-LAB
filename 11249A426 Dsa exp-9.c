9aeea1950bb412ba8fb054a3ed3529a487243cc7
Dhurjati V B K Sai Sushanth <11249a426@kanchiuniv.ac.in>
2025-11-04 12:23:31 +0530

    Add files via upload

---
+++ 11249A426 Dsa exp-9.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+struct node {
+    int data;
+    struct node *left, *right;
+};
+struct node* createNode(int value) {
+    struct node* newNode = (struct node*)malloc(sizeof(struct node));
+    newNode->data = value;
+    newNode->left = newNode->right = NULL;
+    return newNode;
+}
+void preorder(struct node* root) {
+    if (root != NULL) {
+        printf("%d ", root->data);
+        preorder(root->left);
+        preorder(root->right);
+    }
+}
+void inorder(struct node* root) {
+    if (root != NULL) {
+        inorder(root->left);
+        printf("%d ", root->data);
+        inorder(root->right);
+    }
+}
+void postorder(struct node* root) {
+    if (root != NULL) {
+        postorder(root->left);
+        postorder(root->right);
+        printf("%d ", root->data);
+    }
+}
+int main() {
+    struct node* root = createNode(1);
+    root->left = createNode(2);
+    root->right = createNode(3);
+    root->left->left = createNode(4);
+    root->left->right = createNode(5);
+    printf("Preorder Traversal: ");
+    preorder(root);
+    printf("\nInorder Traversal: ");
+    inorder(root);
+    printf("\nPostorder Traversal: ");
+    postorder(root);
+    return 0;
+}
